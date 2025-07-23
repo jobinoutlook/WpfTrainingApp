f0b95ff0a3e4314e05604fcfceb9bc0e6251507f
jobin jose samuel <jobin.samuel@outlook.com>
2025-07-23 12:47:36 +0530

    AppConfig.cs file added

--- WpfCoreApp/App.xaml.cs
+++ WpfCoreApp/App.xaml.cs
@@ -12,18 +12,7 @@ namespace WpfCoreApp
     {
     }
 
-    public static class AppConfig
-    {
-        public static IConfiguration Configuration { get; }
-
-        static AppConfig()
-        {
-            Configuration = new ConfigurationBuilder()
-                .SetBasePath(AppContext.BaseDirectory) //This extension method is in Microsoft.Extensions.Configuration.FileExtensions
-                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true) // This extension method is in Microsoft.Extensions.Configuration.Json
-                .Build();
-        }
-    }
+    
 
 
 
---
+++ WpfCoreApp/AppConfig.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfCoreApp
+{
+    public static class AppConfig
+    {
+        public static IConfiguration Configuration { get; }
+
+        static AppConfig()
+        {
+            Configuration = new ConfigurationBuilder()
+                .SetBasePath(AppContext.BaseDirectory) //This extension method is in Microsoft.Extensions.Configuration.FileExtensions
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true) // This extension method is in Microsoft.Extensions.Configuration.Json
+                .Build();
+        }
+    }
+}
