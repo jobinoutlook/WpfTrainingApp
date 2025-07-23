883af2c277b817e6647b64124bcacfcc1f8c3696
jobin jose samuel <jobin.samuel@outlook.com>
2025-07-23 08:44:09 +0530

    development closed

---
+++ WpfCoreApp/Mvvm/Model/Preferences.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfCoreApp.Mvvm.Model
+{
+    internal class Preferences
+    {
+        public bool ShowInSystemTray {  get; set; }
+        public bool ShowInTaskBar {  get; set; }
+    }
+}
