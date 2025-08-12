bad53358ecdbae123950f3a42e18ca8761750173
jobin jose samuel <jobin.samuel@outlook.com>
2025-08-12 20:42:58 +0530

    Mvvm no user control

---
+++ WpfCoreApp/Mvvm/ViewModel/ContactsVM.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfCoreApp.EF;
+using WpfCoreApp.Mvvm.Model;
+
+namespace WpfCoreApp.Mvvm.ViewModel
+{
+    public class ContactsVM : INotifyPropertyChanged
+    {
+        private bool _isContactsFullyAdded = false;
+        private AppDbContext? _appDbContext = null;
+
+        
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+          PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        }
+
+        private ObservableCollection<Contact> contacts;
+
+        public ObservableCollection<Contact> Contacts
+        {
+            get { return contacts; }
+            set { contacts = value; }
+
+        }
+
+        public ContactsVM()
+        {
+
+            contacts = new ObservableCollection<Contact>();
+            contacts.CollectionChanged += Contacts_CollectionChanged;
+            _appDbContext = new AppDbContext();
+
+            LoadContacts();
+
+            
+
+        }
+
+        private void Contacts_CollectionChanged(object? sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+        {
+            if(_isContactsFullyAdded)
+            {
+                OnPropertyChanged(nameof(Contacts));
+                    
+            }
+        }
+
+        private void LoadContacts()
+        {
+
+
+            contacts.Clear();
+
+            List<Contact> list = _appDbContext.Contacts.ToList();
+
+            if (list.Count == 1)
+            {
+                _isContactsFullyAdded = true;
+                contacts.Add(list[0]);
+                _isContactsFullyAdded = false;
+
+            }
+            else if (list.Count > 1)
+            {
+                for(int i=0; i<list.Count-1; i++)
+                {
+                    contacts.Add(list[i]);
+                }
+
+                _isContactsFullyAdded = true;
+                contacts.Add(list[list.Count - 1]);
+                _isContactsFullyAdded = false;
+
+            }
+
+            
+
+
+        }
+
+    }
+}
