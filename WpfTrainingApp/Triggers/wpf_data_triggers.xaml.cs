f706d2f8b63439dec0572f6ebf75be2f0d86857b
jobin jose samuel <jobin.samuel@outlook.com>
2025-09-16 12:56:50 +0530

    Styles, Triggers, Templates

---
+++ WpfTrainingApp/Triggers/wpf_data_triggers.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfTrainingApp.Triggers
+{
+    /// <summary>
+    /// Interaction logic for wpf_data_triggers.xaml
+    /// </summary>
+    public partial class wpf_data_triggers : Window
+    {
+        public wpf_data_triggers()
+        {
+            InitializeComponent();
+            chk_visible.IsChecked = true;
+        }
+    }
+}
---
+++ WpfTrainingApp/Triggers/wpf_temp_image_butt.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfTrainingApp.Triggers
+{
+    /// <summary>
+    /// Interaction logic for wpf_temp_image_butt.xaml
+    /// </summary>
+    public partial class wpf_temp_image_butt : Window
+    {
+        public wpf_temp_image_butt()
+        {
+            InitializeComponent();
+        }
+    }
+}
