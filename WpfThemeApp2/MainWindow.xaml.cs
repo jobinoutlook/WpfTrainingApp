bc2fabfce584ed324f8894687bf85b4388e96ad9
jobin jose samuel <jobin.samuel@outlook.com>
2025-12-04 11:59:09 +0530

    Window updates

--- WpfThemeApp2/MainWindow.xaml.cs
+++ WpfThemeApp2/MainWindow.xaml.cs
@@ -19,25 +19,71 @@ namespace WpfThemeApp2
         public MainWindow()
         {
             InitializeComponent();
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
+
+            
+            
         }
 
-        private void button_Click(object sender, RoutedEventArgs e)
+        
+
+        void SubWindow_Click(object sender, RoutedEventArgs e)
         {
             SubWindow subWindow = new SubWindow();
-            subWindow.Show();
+            subWindow.Owner = this;
+            subWindow.WindowStartupLocation = WindowStartupLocation.CenterOwner;
+            subWindow.ShowDialog();
         }
 
-        private void button1_Click(object sender, RoutedEventArgs e)
+        
+
+        void LoginWindow_Click(object sender, RoutedEventArgs e)
         {
             LoginWindow loginWindow = new LoginWindow();
             loginWindow.Topmost = true;
+            loginWindow.Owner = this;
+            loginWindow.WindowStartupLocation = WindowStartupLocation.CenterOwner;
             loginWindow.ShowDialog();
         }
 
-        private void button2_Click(object sender, RoutedEventArgs e)
+        
+        void RegularSubWindow_Click(object sender, RoutedEventArgs e)
         {
-            RegularSubWindow subWindow = new RegularSubWindow();
-            subWindow.Show();
+            RegularSubWindow regularWindow = new RegularSubWindow();
+            regularWindow.Owner = this;
+            regularWindow.WindowStartupLocation = WindowStartupLocation.CenterOwner;
+            regularWindow.Show();
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+
+            var result = CenteredMessageBox.Show(this, "Are you sure you want to exit?", "Exit Confirmation", MessageBoxButton.YesNo, MessageBoxImage.Question);
+
+            if (result == MessageBoxResult.Yes)
+            {
+                Application.Current.Shutdown(0);
+
+            }
+            
+        }
+
+        private void Options_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void About_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Canvas_Click(object sender, RoutedEventArgs e)
+        {
+            PaintWindow paintWindow = new PaintWindow();
+            paintWindow.Owner = this;
+            paintWindow.WindowStartupLocation= WindowStartupLocation.CenterOwner;
+            paintWindow.Show();
         }
     }
 }
---
+++ WpfThemeApp2/PaintWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfThemeApp2
+{
+    /// <summary>
+    /// Interaction logic for PaintWindow.xaml
+    /// </summary>
+    public partial class PaintWindow : Window
+    {
+        
+        public PaintWindow()
+        {
+            InitializeComponent();
+            DrawLineOnCanvas();
+        }
+
+        void DrawLineOnCanvas()
+        {
+            Line myLine = new Line
+            {
+                X1 = 5,
+                Y1 = 5,
+                X2 = 200,
+                Y2 = 200,
+                Stroke = Brushes.IndianRed,
+                StrokeThickness = 2
+            };
+            paintCanvas.Children.Add(myLine);
+
+            Polygon polygon = new Polygon
+            {
+                Stroke = Brushes.IndianRed,
+                Fill = Brushes.IndianRed,
+                StrokeThickness = 2,
+                Points = new PointCollection()
+                {
+                    new Point(190,195),
+                    new Point(200,200),
+                    new Point(195,190)
+                }
+            };
+            paintCanvas.Children.Add(polygon);
+
+            Line yaxis = new Line
+            {
+                X1 = 5,
+                Y1 = 5,
+                X2 = 5,
+                Y2 = 200,
+                Stroke = Brushes.Black,
+                StrokeThickness = 2
+            };
+            paintCanvas.Children.Add(yaxis);
+
+            Line xaxis = new Line
+            {
+                X1 = 5,
+                Y1 = 5,
+                X2 = 200,
+                Y2 = 5,
+                Stroke = Brushes.Black,
+                StrokeThickness = 2
+            }; 
+            paintCanvas.Children.Add(xaxis);
+        }
+        
+    }
+}
