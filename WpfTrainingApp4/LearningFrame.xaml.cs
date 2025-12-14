51e6015aa03b13a428367735ad6df44a3720949f
jobin jose samuel <jobin.samuel@outlook.com>
2025-12-14 15:42:49 +0530

    wpf Ultimate projects

---
+++ WpfTrainingApp4/LearningFrame.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfTrainingApp4
+{
+    /// <summary>
+    /// Interaction logic for LearningFrame.xaml
+    /// </summary>
+    public partial class LearningFrame : Window
+    {
+        public LearningFrame()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIntel_Click(object sender, RoutedEventArgs e)
+        {
+            frame1.Visibility = Visibility.Hidden;
+            frame.Visibility = Visibility.Visible;
+        }
+
+        private void btnMsft_Click(object sender, RoutedEventArgs e)
+        {
+            frame.Visibility = Visibility.Hidden;
+            frame1.Visibility = Visibility.Visible;
+        }
+    }
+}
---
+++ Wpf_Ultimate/MainWindow2.xaml.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Wpf_Ultimate
+{
+    /// <summary>
+    /// Interaction logic for MainWindow2.xaml
+    /// </summary>
+    public partial class MainWindow2 : Window
+    {
+        public MainWindow2()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOne_MouseEnter(object sender, MouseEventArgs e)
+        {
+            gridlayer1.Visibility = Visibility.Visible;
+
+            // Adjust ZIndex order to ensure the pane is on top:
+            parentGrid.Children.Remove(gridlayer1);
+            parentGrid.Children.Add(gridlayer1);
+
+            // Ensure the other pane is hidden if it is undocked
+            if (btnTwo.Visibility == Visibility.Visible)
+                gridlayer2.Visibility = Visibility.Collapsed;
+        }
+
+        private void btnTwo_MouseEnter(object sender, MouseEventArgs e)
+        {
+            gridlayer2.Visibility = Visibility.Visible;
+
+            // Adjust ZIndex order to ensure the pane is on top:
+            parentGrid.Children.Remove(gridlayer2);
+            parentGrid.Children.Add(gridlayer2);
+
+            // Ensure the other pane is hidden if it is undocked
+            if (btnOne.Visibility == Visibility.Visible)
+                gridlayer1.Visibility = Visibility.Collapsed;
+        }
+
+        private void panel1_MouseEnter(object sender, MouseEventArgs e)
+        {
+            // Ensure the other pane is hidden if it is undocked
+            if (btnTwo.Visibility == Visibility.Visible)
+                gridlayer2.Visibility = Visibility.Collapsed;
+        }
+
+        private void panel2_MouseEnter(object sender, MouseEventArgs e)
+        {
+            // Ensure the other pane is hidden if it is undocked
+            if (btnOne.Visibility == Visibility.Visible)
+                gridlayer1.Visibility = Visibility.Collapsed;
+        }
+
+        private void panel1Pin_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        
+
+        private void panel2Pin_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void layer0_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if (btnOne.Visibility == Visibility.Visible)
+                gridlayer1.Visibility = Visibility.Collapsed;
+            if (btnTwo.Visibility == Visibility.Visible)
+                gridlayer2.Visibility = Visibility.Collapsed;
+        }
+
+        private void parentGrid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                if (btnOne.Visibility == Visibility.Visible)
+                    gridlayer1.Visibility = Visibility.Collapsed;
+                if (btnTwo.Visibility == Visibility.Visible)
+                    gridlayer2.Visibility = Visibility.Collapsed;
+            }
+            
+        }
+    }
+}
