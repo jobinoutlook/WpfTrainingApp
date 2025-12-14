64c4c1c1a724a1505c5a51ad093d7eab83771a7e
jobin jose samuel <jobin.samuel@outlook.com>
2025-12-14 20:18:34 +0530

    docking enabled

--- Wpf_Ultimate/MainWindow2.xaml.cs
+++ Wpf_Ultimate/MainWindow2.xaml.cs
@@ -19,9 +19,29 @@ namespace Wpf_Ultimate
     /// </summary>
     public partial class MainWindow2 : Window
     {
+        bool panel1_mouseleave = false;
+        bool panel1_mouseenter=false;
+
+        bool panel2_mouseleave = false;
+        bool panel2_mouseenter = false;
+
+        bool parentGrid_mouseenter=false;
+        bool parentGrid_mouseleave=false;
+
+        ColumnDefinition colOneCopyForLayer0;
+        ColumnDefinition colTwoCopyForLayer0;
+        ColumnDefinition colTwoCopyForLayer1;
+
         public MainWindow2()
         {
             InitializeComponent();
+            // Initialize the dummy (grouped) columns that are created during docking:
+            colOneCopyForLayer0 = new ColumnDefinition();
+            colOneCopyForLayer0.SharedSizeGroup = "column1";
+            colTwoCopyForLayer0 = new ColumnDefinition();
+            colTwoCopyForLayer0.SharedSizeGroup = "column2";
+            colTwoCopyForLayer1 = new ColumnDefinition();
+            colTwoCopyForLayer1.SharedSizeGroup = "column2";
         }
 
         private void btnOne_MouseEnter(object sender, MouseEventArgs e)
@@ -52,9 +72,23 @@ namespace Wpf_Ultimate
 
         private void panel1_MouseEnter(object sender, MouseEventArgs e)
         {
+            panel1_mouseenter = true;
+            panel1_mouseleave = false;
             // Ensure the other pane is hidden if it is undocked
             if (btnTwo.Visibility == Visibility.Visible)
                 gridlayer2.Visibility = Visibility.Collapsed;
+        }
+
+        private void panel1_MouseLeave(object sender, MouseEventArgs e)
+        {
+            panel1_mouseleave=true;
+            panel1_mouseenter=false;
+
+            if(parentGrid_mouseenter==true)
+            {
+                if (btnOne.Visibility == Visibility.Visible)
+                    gridlayer1.Visibility = Visibility.Collapsed;
+            }
         }
 
         private void panel2_MouseEnter(object sender, MouseEventArgs e)
@@ -62,18 +96,27 @@ namespace Wpf_Ultimate
             // Ensure the other pane is hidden if it is undocked
             if (btnOne.Visibility == Visibility.Visible)
                 gridlayer1.Visibility = Visibility.Collapsed;
+
+
+            panel2_mouseenter = true;
+            panel2_mouseleave = false;
         }
 
-        private void panel1Pin_Click(object sender, RoutedEventArgs e)
+        public void panel1Pin_Click(object sender, RoutedEventArgs e)
         {
-
+            if (btnOne.Visibility == Visibility.Collapsed)
+                UndockPane(1);
+            else
+                DockPane(1);
         }
 
-        
-
-        private void panel2Pin_Click(object sender, RoutedEventArgs e)
+        // Toggle panel 2 between docked and undocked states 
+        public void panel2Pin_Click(object sender, RoutedEventArgs e)
         {
-
+            if (btnTwo.Visibility == Visibility.Collapsed)
+                UndockPane(2);
+            else
+                DockPane(2);
         }
 
         private void layer0_MouseEnter(object sender, MouseEventArgs e)
@@ -87,14 +130,113 @@ namespace Wpf_Ultimate
         private void parentGrid_MouseDown(object sender, MouseButtonEventArgs e)
         {
             
-            if (e.LeftButton == MouseButtonState.Pressed)
+            //if (e.LeftButton == MouseButtonState.Pressed)
+            //{
+            //    if (panel1_mouseleave==true && panel1_mouseenter==false)
+            //    {
+            //        if (btnOne.Visibility == Visibility.Visible)
+            //            gridlayer1.Visibility = Visibility.Collapsed;
+            //    }
+
+            //    if (panel2_mouseleave == true && panel2_mouseenter == false)
+            //    {
+            //        if (btnTwo.Visibility == Visibility.Visible)
+            //            gridlayer2.Visibility = Visibility.Collapsed;
+            //    }
+            //}
+            
+        }
+
+        private void panel2_MouseLeave(object sender, MouseEventArgs e)
+        {
+
+
+            panel2_mouseleave = true;
+            panel2_mouseenter = false;
+
+            if (parentGrid_mouseenter == true)
             {
-                if (btnOne.Visibility == Visibility.Visible)
-                    gridlayer1.Visibility = Visibility.Collapsed;
                 if (btnTwo.Visibility == Visibility.Visible)
                     gridlayer2.Visibility = Visibility.Collapsed;
             }
-            
+        }
+
+        private void parentGrid_MouseEnter(object sender, MouseEventArgs e)
+        {
+            parentGrid_mouseenter = true;
+            parentGrid_mouseleave = false;
+          
+        }
+
+        private void parentGrid_MouseLeave(object sender, MouseEventArgs e)
+        {
+            //if (parentGrid_mouseenter)
+            //{
+            //    if (panel1_mouseleave == true)// && panel1_mouseenter == false)
+            //    {
+            //        if (btnOne.Visibility == Visibility.Visible)
+            //            gridlayer1.Visibility = Visibility.Collapsed;
+            //    }
+
+            //    if (panel2_mouseleave == true)// && panel2_mouseenter == false)
+            //    {
+            //        if (btnTwo.Visibility == Visibility.Visible)
+            //            gridlayer2.Visibility = Visibility.Collapsed;
+            //    }
+            //}
+
+            parentGrid_mouseenter = false;
+            parentGrid_mouseleave = true;
+        }
+
+        public void DockPane(int paneNumber)
+        {
+            if (paneNumber == 1)
+            {
+                btnOne.Visibility = Visibility.Collapsed;
+                panel1PinImg.Source = new BitmapImage(new Uri("VerticalPin.jpg", UriKind.Relative));
+
+                // Add the cloned column to layer 0:
+                layer0.ColumnDefinitions.Add(colOneCopyForLayer0);
+                // Add the cloned column to layer 1, but only if pane 2 is docked:
+                if (btnTwo.Visibility == Visibility.Collapsed) gridlayer1.ColumnDefinitions.Add(colTwoCopyForLayer1);
+            }
+            else if (paneNumber == 2)
+            {
+                btnTwo.Visibility = Visibility.Collapsed;
+                panel2PinImg.Source = new BitmapImage(new Uri("VerticalPin.jpg", UriKind.Relative));
+
+                // Add the cloned column to layer 0:
+                layer0.ColumnDefinitions.Add(colTwoCopyForLayer0);
+                // Add the cloned column to layer 1, but only if pane 1 is docked:
+                if (btnOne.Visibility == Visibility.Collapsed) gridlayer1.ColumnDefinitions.Add(colTwoCopyForLayer1);
+            }
+        }
+
+        // Undocks a pane, which reveals the corresponding pane button
+        public void UndockPane(int panelNbr)
+        {
+            if (panelNbr == 1)
+            {
+                gridlayer1.Visibility = Visibility.Collapsed;
+                btnOne.Visibility = Visibility.Visible;
+                panel1PinImg.Source = new BitmapImage(new Uri("HorizontalPin.jpg", UriKind.Relative));
+
+                // Remove the cloned columns from layers 0 and 1:
+                layer0.ColumnDefinitions.Remove(colOneCopyForLayer0);
+                // This won't always be present, but Remove silently ignores bad columns:
+                gridlayer1.ColumnDefinitions.Remove(colTwoCopyForLayer1);
+            }
+            else if (panelNbr == 2)
+            {
+                gridlayer2.Visibility = Visibility.Collapsed;
+                btnTwo.Visibility = Visibility.Visible;
+                panel2PinImg.Source = new BitmapImage(new Uri("HorizontalPin.jpg", UriKind.Relative));
+
+                // Remove the cloned columns from layers 0 and 1:
+                layer0.ColumnDefinitions.Remove(colTwoCopyForLayer0);
+                gridlayer1.ColumnDefinitions.Remove(colTwoCopyForLayer1);
+            }
         }
     }
 }
